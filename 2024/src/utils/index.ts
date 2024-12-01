06a23ad8b1331399972625b7912305bb08b81584
Dani Lauzurica <danilau11@gmail.com>
2024-12-01 12:01:59 +0100

    chore: setup 2024

---
+++ 2024/src/utils/index.ts
@@ -0,0 +1,40 @@
+import fs from "fs";
+
+type ReadInputParams = {
+  test?: boolean;
+  split?: boolean;
+  clear?: boolean;
+};
+
+export const readInput = <T extends ReadInputParams>(opts: T) => {
+  const TEST_INPUT_FILENAME = "./testinput.txt";
+  const INPUT_FILENAME = "./input.txt";
+
+  if (opts.clear) {
+    console.clear();
+  }
+
+  const filename = opts.test ? TEST_INPUT_FILENAME : INPUT_FILENAME;
+
+  const rawInput = fs.readFileSync(filename, { encoding: "utf8", flag: "r" });
+
+  return opts.split
+    ? (rawInput.split("\n") as T["split"] extends true ? string[] : string)
+    : (rawInput as T["split"] extends true ? string[] : string);
+};
+
+export const assertResult = (expected: any, actual: any) =>
+  console.assert(
+    expected === actual,
+    `\nExpected: ${expected}\nActual: ${actual}`,
+  );
+
+export const zip = <T extends any[]>(
+  ...arrays: { [K in keyof T]: T[K][] }
+): { [K in keyof T]: T[K] }[] => {
+  const minLength = Math.min(...arrays.map((arr) => arr.length));
+  return Array.from(
+    { length: minLength },
+    (_, i) => arrays.map((arr) => arr[i]) as { [K in keyof T]: T[K] },
+  );
+};
